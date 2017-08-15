4242ae3d9724efd6cd9b7dbc41cfa4bfa2fd1018
tim degroot <tdegroot4390@gmail.com>
2017-08-15 14:37:09 +0100

    genrated project controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,39 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = current_user.projects.all
+  end
+
+  def show
+    @project = params[:id]
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Projet.new(project_params)
+    @project.user_id = current_user.id
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :schedule, :location)
+  end
+
+end
