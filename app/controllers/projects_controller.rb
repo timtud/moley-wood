b7b93f8c4d8c8119798958176516095161780d93
tim degroot <tdegroot4390@gmail.com>
2017-08-15 14:53:09 +0100

    changed routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,7 +15,8 @@ class ProjectsController < ApplicationController
   def create
     @project = Projet.new(project_params)
     @project.user_id = current_user.id
-
+    if @project.save
+      redirect_to :
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show ] do
-    resources :projects, only:[ :new, :create, :show, :index]
-    resources :user_skills, only: [ :create, :update]
-  end
+  resources :users, only: [:show ]
 
+  resources :user_skills, only: [ :create, :update]
 
-  resources :projects, only: [  :destroy, :edit, :update] do
+  resources :projects, only: [ :new, :create, :show, :index, :destroy, :edit, :update] do
     resources :jobs, only: [ :create]
     resources :host_reviews, only: [:create]
   end
