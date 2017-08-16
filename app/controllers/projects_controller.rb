8253c74590c67e218e287e62ebfb06072777fd6e
tim degroot <tdegroot4390@gmail.com>
2017-08-16 15:44:42 +0100

    made landing page animation and fixed some bugs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,15 +3,12 @@ class ProjectsController < ApplicationController
    def index
 
     if params[:search]
-
       @projects = Project.search(params[:search]).order("created_at DESC")
     # elsif params[:search]
     #   @projects = Project.search(params[:search2]).order("created_at DESC")
     else
       @projects = Project.all.order('created_at DESC')
     end
-
-
   end
 
 
@@ -20,9 +17,9 @@ class ProjectsController < ApplicationController
     @job = Job.new
   end
 
-  # def new
-  #   @project = Project.new
-  # end
+  def new
+    @project = Project.new
+  end
 
 
   def create
@@ -31,26 +28,26 @@ class ProjectsController < ApplicationController
     if @project.save
       redirect_to project_path(@project)
     else
-      render 'project/new'
+      render 'projects/new'
     end
   end
 
-  # def edit
+  def edit
 
-  # end
+  end
 
-  # def update
+  def update
 
-  # end
+  end
 
-  # def destroy
+  def destroy
 
-  # end
+  end
 
-  # private
+  private
 
-  # def project_params
-  #   params.require(:project).permit(:title, :description, :schedule, :location)
-  # end
+  def project_params
+    params.require(:project).permit(:title, :description, :schedule, :location)
+  end
 
 end
