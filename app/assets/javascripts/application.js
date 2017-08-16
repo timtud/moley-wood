c9dea8d20d56424074633ab7bc451a1a74689730
tim degroot <tdegroot4390@gmail.com>
2017-08-16 16:03:13 +0100

    get ready for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,12 +4,12 @@
 //= require_tree .
 
 
-$(document).ready(function() {
-$("body").keyup(function(e){
-  if(e.keyCode === 77)
-    $("body").toggleClass("mole");
-});
-  $("body").mousemove(function(e){
-    $("body").css('background-position',(e.pageX - 250)+'px '+(e.pageY - 250)+'px');
-  });
-});
+// $(document).ready(function() {
+// $("body").keyup(function(e){
+//   if(e.keyCode === 77)
+//     $("body").toggleClass("mole");
+// });
+//   $("body").mousemove(function(e){
+//     $("body").css('background-position',(e.pageX - 250)+'px '+(e.pageY - 250)+'px');
+//   });
+// });
