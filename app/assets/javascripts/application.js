3ffc0f163051f7228f91d6a47bcad9def593b220
Tobias Braendli <tobiasbraendli@gmail.com>
2017-08-17 19:19:27 +0100

    new features from tobi

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+
+//= require jquery-fileupload/basic
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
+
+
 //= require_tree .
+
 
 
 // $(document).ready(function() {
