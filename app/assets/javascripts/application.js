0bd2ed1ccde096b1acc0bf0599a7e2ceca87fc08
Goncalo Novo <goncalo.novo@protonmail.com>
2017-08-18 16:49:35 +0100

    added stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,12 @@
 
 
 
-// $(document).ready(function() {
-// $("body").keyup(function(e){
-//   if(e.keyCode === 77)
-//     $("body").toggleClass("mole");
-// });
-//   $("body").mousemove(function(e){
-//     $("body").css('background-position',(e.pageX - 250)+'px '+(e.pageY - 250)+'px');
-//   });
-// });
+$(document).ready(function() {
+$("body").keyup(function(e){
+  if(e.keyCode === 77)
+    $("body").toggleClass("mole");
+});
+  $("body").mousemove(function(e){
+    $("body").css('background-position',(e.pageX - 250)+'px '+(e.pageY - 250)+'px');
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
  # devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  resources :pages, only: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
 
